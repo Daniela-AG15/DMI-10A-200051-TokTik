43ded57911c2ff6fd35b11fa0324d9b97e5214c7
Daniela-AG15 <danaguilar1512@gmail.com>
2024-11-26 09:33:23 -0600

    Practica24

--- toktik_200051/android/app/src/main/kotlin/com/example/toktik_210084/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.toktik_210084
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- toktik_200051/lib/main.dart
+++ toktik_200051/lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import 'package:toktik_200051/config/theme/app_theme.dart';
-import 'package:toktik_200051/presentation/providers/discover_provider.dart';
-import 'package:toktik_200051/presentation/screens/discover/discover_screen.dart';
+import 'package:toktik/config/theme/app_theme.dart';
+import 'package:toktik/presentation/providers/discover_provider.dart';
+import 'package:toktik/presentation/screens/discover/discover_screen.dart';
 
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider(create: (_) => DiscoverProvider())
+        ChangeNotifierProvider( 
+          lazy: false,
+          create: (_) => DiscoverProvider()..loadNextPage() 
+        ),
       ],
       child: MaterialApp(
         title: 'TokTik',
